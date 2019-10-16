0f71d137f39849afad2dbeb8419f10c31be4f53d
Guest User <Guest@imac-06.local>
2019-10-16 19:24:48 +0200

    Commit

--- weather/ViewController.swift
+++ weather/ViewController.swift
@@ -9,12 +9,41 @@
 import UIKit
 
 class ViewController: UIViewController {
+    let task = session.dataTask(with: url) { data, response, error in
+        
+        guard error == nil else {
+            print ("error: \(error!)")
+            return
+        }
+        
+        // ensure there is data returned from this HTTP response
+        guard let content = data else {
+            print("No data")
+            return
+        }
+        
+        // serialise the data / NSData object into Dictionary [String : Any]
+        guard let json = (try? JSONSerialization.jsonObject(with: content, options: JSONSerialization.ReadingOptions.mutableContainers)) as? [String: Any] else {
+            print("Not containing JSON")
+            return
+        }
+        
+        print("gotten json response dictionary is \n \(json)")
+        print("gotten json response dictionary is \n \(json)")
+        
+        
+
+        // update UI using the response here
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        task.resume();
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
 
 }
+
 
